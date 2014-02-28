d1527da477a85471404061002c4555dbfddde392
Thorgar <nstommel@outlook.de>
2014-02-28 22:15:00 +0100

    Basic libraries implemented
    I implemented some libraries to manage the game loop, get a fiexed
    framerate , and show the fps.
    I also renamed the mainLoop.js in GameLogic, to avoid confusion with the
    GameLoopManager.js library

---
+++ scripts/GameLogic.js
@@ -0,0 +1,50 @@
+// ----------------------------------------
+// Actual game code goes here.
+
+// Global vars
+fps = null; 
+canvas = null;
+ctx = null;
+
+// ----------------------------------------
+
+// Our 'game' variables
+var posX = 0;
+var posY = 0;
+var velX = 100;
+var velY = 100;
+var sizeX = 80;
+var sizeY = 40;
+
+function GameTick(elapsed)
+{
+    fps.update(elapsed);
+
+    // --- Logic
+
+    // Movement physics
+    posX += velX*elapsed;
+    posY += velY*elapsed;
+    // Collision detection and response
+    if ( (posX <= 0 && velX < 0) || (posX >= canvas.width-sizeX && velX > 0) )
+        velX = -velX;
+    if ( (posY <= 0 && velY < 0) || (posY >= canvas.height-sizeY && velY > 0) )
+        velY = -velY;
+
+    // --- Rendering
+
+    // Clear the screen
+    ctx.fillStyle = "cyan";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // Render objects
+    ctx.strokeRect(posX, posY, sizeX, sizeY);
+    ctx.fillStyle = "red";
+    ctx.fillText("Hello Mike!", posX+10, posY+25);
+}
+
+window.onload = function () {
+    canvas = document.getElementById("level");
+    ctx = canvas.getContext("2d");
+    fps = new FPSMeter("fpsmeter", document.getElementById("fpscontainer"));
+    GameLoopManager.run(GameTick);
+};
